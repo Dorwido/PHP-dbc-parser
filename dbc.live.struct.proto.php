9f4f4415233759d699a7d30b9be2d89aac805636
Dorwido <darkz@gmx.de>
2011-08-18 17:54:38 +0200

    added structures for faction,itemclass,itemsubclass,skilline , you can just hit the "Parse DBC data" after configuration to extract those ones.

--- dbc.live.struct.proto.php
+++ dbc.live.struct.proto.php
@@ -11,6 +11,73 @@ class dbcstructs{
 		}else{
 			die("No definition found for ".$struct."\r\n");
 		}
-	}	
+	}	function Faction(){
+		$returndata = array (
+			$this->GetInt('FactionId','primary'),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetString('FactionName','en'),
+			$this->GetUnknown(),
+			$this->GetUnknown()
+		);
+		return $returndata;
+	}	function ItemClass(){
+		$returndata = array (
+			$this->GetUnknown(),
+			$this->GetInt('itemClass','primary'),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetString('itemClassName','en'),
+		);
+		return $returndata;
+	}	function ItemSubClass(){
+		$returndata = array (
+			$this->GetUnknown(),
+			$this->GetInt('itemClass','primary'),
+			$this->GetInt('itemSubClass','primary'),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetString('shorttext','en'),
+			$this->GetString('longtext','en')
+		);
+		return $returndata;
+	}	function SkillLine(){
+		$returndata = array (
+			$this->GetInt('SkillId','primary'),
+			$this->GetUnknown(),
+			$this->GetString('SkillName','en'),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown(),
+			$this->GetUnknown()
+		);
+		return $returndata;
+	}
 }
 ?>
